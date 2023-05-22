5cb294a8150074b0ceee8038be87ac27f030b840
ajayjhadev <ajaykumarjha05072000@gmail.com>
2023-05-22 14:35:48 +0530

    m

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,4 @@
 import mongoose from "mongoose";
-import validator from "validator";
-import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
   fullName: {
@@ -22,7 +20,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Email required"],
     lowercase: true,
-    validate: [validator.isEmail, "Email format incorrect"],
   },
   password: {
     type: String,
@@ -32,48 +29,12 @@ const userSchema = new mongoose.Schema({
   passwordConfirmation: {
     type: String,
     required: [true, "Confirm password needed"],
-    validate: {
-      validator: function (el) {
-        return el === this.password;
-      },
-      message: "Password don't match",
-    },
   },
-  passwordChangedAt: {
-    type: Date,
-    default: Date.now,
+  createdDate: {
+    type: String,
+    default: new Date().toJSON().slice(0, 10).split("-").reverse().join("-"),
   },
-
-  passwordResetToken: String,
-  passwordResetExpiry: Date,
 });
-
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) next();
-  this.password = await bcrypt.hash(this.password, 12);
-  this.passwordChangedAt = Date.now() - 2000;
-  this.passwordConfirmation = undefined;
-  next();
-});
-
-// instance method
-userSchema.methods.verifyPassword = async function (
-  userPassword,
-  encryptedPassword
-) {
-  return await bcrypt.compare(userPassword, encryptedPassword);
-};
-
-userSchema.methods.invalidateTokens = function (tokeniat) {
-  if (this.passwordChangedAt) {
-    const tokenTimeStamp = new Date(tokeniat * 1000);
-    return this.passwordChangedAt > tokenTimeStamp;
-  }
-
-  return false;
-};
-
-userSchema.methods.createResetToken = function () {};
 
 const User = mongoose.model("User", userSchema);
 
